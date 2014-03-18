a78ba16eadfd820609606eba2290011366407852
Velocitycoin <velocitycoin@hotmail.com>
2014-03-18 12:55:48 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -25,10 +25,10 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90006;
+static const int PROTOCOL_VERSION = 90007;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 90002;
+static const int MIN_PROTO_VERSION = 90003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -36,7 +36,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60000;
-static const int NOBLKS_VERSION_END = 89999;
+static const int NOBLKS_VERSION_END = 90003;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
@@ -45,8 +45,8 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       1
-#define DISPLAY_VERSION_MINOR       1
-#define DISPLAY_VERSION_REVISION    3
+#define DISPLAY_VERSION_MINOR       2
+#define DISPLAY_VERSION_REVISION    0
 #define DISPLAY_VERSION_BUILD       0
 
 #endif
